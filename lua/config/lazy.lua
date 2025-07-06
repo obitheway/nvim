130e11dfe6bdc09d17d754e6d400d764b8f68b7d
obitheway <obi@obigeorge.com>
2025-07-06 19:55:01 -0400

    Added Harpoon back to the config
    Doesn't look pretty, clean it up later

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,15 +21,46 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
     -- Add Telescope plugin
-    { "nvim-telescope/telescope.nvim",
+    {
+      "nvim-telescope/telescope.nvim",
       dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
       },
       config = function()
-        require("telescope").setup {
+        require("telescope").setup({
           --your Telescope configuration here, when needed.
-        }
-      end
+        })
+      end,
+    },
+    {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local harpoon = require("harpoon")
+        harpoon:setup()
+
+        -- Example keymaps (you can customize these)
+        local keymap = vim.keymap.set
+        keymap("n", "<leader>a", function()
+          harpoon:list():append()
+        end, { desc = "Harpoon Add File" })
+        keymap("n", "<leader>h", function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end, { desc = "Harpoon Quick Menu" })
+        keymap("n", "<leader>1", function()
+          harpoon:list():select(1)
+        end, { desc = "Harpoon to File 1" })
+        keymap("n", "<leader>2", function()
+          harpoon:list():select(2)
+        end, { desc = "Harpoon to File 2" })
+        keymap("n", "<leader>3", function()
+          harpoon:list():select(3)
+        end, { desc = "Harpoon to File 3" })
+        keymap("n", "<leader>4", function()
+          harpoon:list():select(4)
+        end, { desc = "Harpoon to File 4" })
+      end,
     },
   },
   defaults = {
